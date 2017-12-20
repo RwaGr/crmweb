3839885df1764779375e37e6dd0499a436109d7c
RwaGr <rwagraltern@gmail.com>
2017-12-20 17:19:21 -0300

    Add files via upload

--- modelos/Contacto.php
+++ modelos/Contacto.php
@@ -11,33 +11,19 @@ Class Contacto
 	}
 
 	//Implementamos un método para insertar registros
-	public function insertar($razonsocial,$tlf_1,$correo,$filtro,$estado)
+	public function insertar($razonsocial,$contacto,$tlf_1,$correo,$filtro,$estado)
 	{
-		$sql="INSERT INTO contacto (razonsocial,tlf_1,correo,filtro,estado)
-		VALUES ('$razonsocial','$tlf_1','$correo','$filtro','$estado')";
+		$sql="INSERT INTO contacto (razonsocial,contacto,tlf_1,correo,filtro,estado)
+		VALUES ('$razonsocial', '$contacto','$tlf_1','$correo','$filtro','$estado')";
 		return ejecutarConsulta($sql);
 	}
 
 	//Implementamos un método para editar registros
-	public function editar($idcontacto,$razonsocial,$tlf_1,$correo,$filtro,$estado)
+	public function editar($idcontacto,$razonsocial,$rut,$giro,$contacto,$cargo,$tlf_1,$tlf_2,$correo,$region,$ciudad,$comuna,$direccion,$canal_deingreso,$filtro,$estado,$enlace,$fec_ingreso,$codigopostal,$imagen,$sitioweb)
 	{
-		$sql="UPDATE contacto SET razonsocial='$razonsocial',tlf_1='$tlf_1',correo='$correo',filtro='$filtro',estado='$estado' WHERE idcontacto='$idcontacto'";
+		$sql="UPDATE contacto SET razonsocial='$razonsocial',rut='$rut',giro='$giro',contacto='$contacto',cargo='$cargo',tlf_1='$tlf_1',tlf_2='$tlf_2',correo='$correo',region='$region',ciudad='$ciudad',comuna='$comuna',direccion='$direccion',canal_deingreso='$canal_deingreso',filtro='$filtro',estado='$estado'enlace='$enlace', fec_ingreso='$fec_ingreso', codigopostal='$codigopostal', imagen='$imagen', sitioweb='$sitioweb' WHERE idcontacto='$idcontacto'";
 		return ejecutarConsulta($sql);
 	}
-
-	/** Implementamos un método para desactivar categorías
-	public function desactivar($idcontacto)
-	{
-		$sql="UPDATE contacto SET condicion='0' WHERE idcontacto='$idcontacto'";
-		return ejecutarConsulta($sql);
-	}
-
-	//Implementamos un método para activar categorías
-	public function activar($idcontacto)
-	{
-		$sql="UPDATE contacto SET condicion='1' WHERE idcontacto='$idcontacto'";
-		return ejecutarConsulta($sql);
-	}*/
 
 	//Implementar un método para mostrar los datos de un registro a modificar
 	public function mostrar($idcontacto)
