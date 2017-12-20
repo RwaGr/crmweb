eb131724c938281791a794429773ea088823fc97
RwaGr <rwagraltern@gmail.com>
2017-12-20 17:18:45 -0300

    Add files via upload

--- ajax/mostrar.php
+++ ajax/mostrar.php
@@ -51,10 +51,11 @@ while ($reg=$rspta->fetch_object()){
     $data[]=array(
         "0"=>'<button class="btn btn-primary" data-toggle="modal" data-target="#modalMostrar" onclick="mostrarModal('.$reg->idcontacto.')"><i class="fa fa-fw fa-search"></i></button>'.'<button class="btn btn-warning" data-toggle="modal" data-target="#modalEdit" onclick="editarModal('.$reg->idcontacto.')"><i class="fa fa-fw fa-edit"></i></button>',
         "1"=>$reg->razonsocial,
-        "2"=>$reg->tlf_1,
-        "3"=>$reg->correo,
-        "4"=>$valor,
-        "5"=>$estadocolor
+        "2"=>$reg->contacto,
+        "3"=>$reg->tlf_1,
+        "4"=>$reg->correo,
+        "5"=>$valor,
+        "6"=>$estadocolor
     );
 }
 $results = array(
